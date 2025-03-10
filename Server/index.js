4323b8229976cdc3a70dd432ee3934e593f98585
nsunyer <nsunyer@gmail.com>
2025-03-10 17:21:45 +0100

    Firebase

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,41 @@
 
-alumnes = [{nom: "Pere", edat: "15"}, {nom: "Maria", edat: "16"}];
-
 const express = require('express');
 
 const app = express();
 
 app.use(express.static('www/browser'));
 
-app.get('/api/', (req, res) => {
-    res.sent('Hello World!');
+const fs = require('firebase-admin');
+const serviceAccount = require('./key.json');
+
+fs.initializeApp({ credential: fs.credential.cert(serviceAccount) });
+const db = fs.firestore();
+
+app.get('/api/alumne/:id', async (req, res) => {
+    try {
+       const alumneRef = db.collection ("alumnes").doc(req.params.id);
+       const response = await alumneRef.get();
+       res.send(response.data()); 
+    } catch(error) {
+        res.send(error);
+    }
 });
 
-app.get('/api/alumnes', (req, res) => {
-    res.send(JSON.stringify(alumnes));
+app.get('/api/alumnes', async (req, res) => {
+    try {
+        const docsRef = await db.collection ("alumnes");
+        const mainDocs = [];
+
+        const docs = await docsRef.get();
+
+        docs.forEach(async (doc) => {
+            mainDocs.push({ ...doc.data(), _id: doc.id });
+        });
+        
+        res.send(mainDocs); 
+     } catch(error) {
+         res.send(error);
+     }
 });
 
 const port = 3000;
